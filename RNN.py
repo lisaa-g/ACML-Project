44b0f02a00931a899779928e03c1fe777436d98e
nihal-ranchod <nihal.ranchod@gmail.com>
2024-05-14 19:43:31 +0200

    change epochs

--- RNN.py
+++ RNN.py
@@ -72,7 +72,7 @@ for mean, stdev, param in zip(means, stds, params):
 
 # Train the model using the best parameters found
 model = create_model(optimizer=grid_result.best_params_['optimizer'])
-history = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=32)
+history = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=50, batch_size=32)
 
 # Plot the training history
 plt.figure(figsize=(12, 4))
